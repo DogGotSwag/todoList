24153c30d82fa0898d0683c30b1ba1defed5a84b
Didier <115608922+DogGotSwag@users.noreply.github.com>
2024-05-29 10:19:25 -0600

    install loaders for css and implement in config
    now i can import css files into js files

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,4 +4,12 @@ const common = require('./webpack.config.js');
 module.exports = merge( common , {
     mode: "development",
     devtool: "inline-source-map",
+    module : {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
 });
