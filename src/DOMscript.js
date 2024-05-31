0a6a2dff5d654b2a404b0d7aaec72b05d5f5ee5f
Didier <115608922+DogGotSwag@users.noreply.github.com>
2024-05-31 12:34:31 -0600

    add to popup that creates projects;
    it has input for user to type out project name

--- src/DOMscript.js
+++ src/DOMscript.js
@@ -15,12 +15,53 @@ let DOM_Module = ( function(){
     let leftSide = document.querySelector('.left')
     let projectForm =() => {
         let formContainer = document.createElement('div');
-        formContainer.innerText = 'hello';
         formContainer.style.cssText = `
+            display: flex;
+            gap: 6px;
+            flex-wrap:wrap;
+            justify-content: center;
+            align-content: center;
+
             background-color: blue;
             position: absolute;
-            width: 90%; 
+            width: 80%;
+            height: auto;
+            border: 1px solid black;
+            border-radius: 6px 2px 6px 6px;
+            background-color: whitesmoke;
+            right: 30px;
+            top: 19px;
+            padding: 10px;
         `;
+
+        let label = document.createElement("label");
+        let input = document.createElement("input");
+        let p = document.createElement("p");
+        let button = document.createElement("button");
+
+        label.style.cssText = `
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        `;
+
+        input.style.cssText = `
+            padding: 2px 10px;
+            height: 18px;
+        `;
+
+        p.innerText = "Project Name";
+        button.innerText = "Submit";
+
+        button.style.cssText = `
+            width: 50px;
+        `;
+
+        label.appendChild(p);
+        formContainer.appendChild(label);
+        formContainer.appendChild(input);
+        formContainer.appendChild(button);
+
         leftSide.appendChild(formContainer);
 
 
